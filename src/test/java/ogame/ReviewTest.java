c85165c440162054ea416e3a8eba48fee0c52ee8
ruslan.bilyi <beliysacred@gmail.com>
2016-10-16 19:27:06 +0300

    preparation for first run

--- src/test/java/ogame/ReviewTest.java
+++ src/test/java/ogame/ReviewTest.java
@@ -2,16 +2,14 @@ package ogame;
 
 import org.junit.Test;
 import preparation.BeforeClass;
-import signIn.SignInPage;
 
 /**
  * Created by Sacred on 16.10.2016.
  */
-public class ReviewTest extends BeforeClass{
+public class ReviewTest extends BeforeClass {
 
 
     @Test
     public void review() throws InterruptedException {
-        Thread.sleep(3000);
     }
 }
--- src/test/java/preparation/BeforeClass.java
+++ src/test/java/preparation/BeforeClass.java
@@ -1,9 +1,13 @@
 package preparation;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.junit.ScreenShooter;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
 import signIn.SignInPage;
+
+import java.io.IOException;
 
 import static com.codeborne.selenide.Selenide.close;
 import static com.codeborne.selenide.Selenide.open;
@@ -13,27 +17,26 @@ import static com.codeborne.selenide.Selenide.open;
  */
 public class BeforeClass {
 
-    SignInPage signInPage = new SignInPage();
-
-    private final String baseURL = "https://ru.ogame.gameforge.com/";
     final String browserPropertyName = "webdriver.chrome.driver";
     final String browserPropertyPath = "./src/main/resources/chromedriver.exe";
     final int timeOut = 10000;
+    private final String baseURL = "https://ru.ogame.gameforge.com/";
+    @Rule
+    public ScreenShooter makeScreenshotOnFailure = ScreenShooter.failedTests().succeededTests();
+    SignInPage signInPage = new SignInPage();
 
     @Before
     public void before() {
         Configuration.browser = "chrome";
         Configuration.timeout = timeOut;
+        makeScreenshotOnFailure.captureSuccessfulTests = true;
         System.setProperty(browserPropertyName, browserPropertyPath);
         open(baseURL);
-        signInPage.clickOnLoginButton();
-        signInPage.chooseServer("Izar");
+        signInPage.completeLogin("Izar", "checker", "Testtest02");
     }
 
-
-
     @After
-    public void after() {
+    public void after() throws IOException {
         close();
     }
 }
