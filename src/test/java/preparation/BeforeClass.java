e33b486a86ba379352c5e92719fe60cb79fca7e9
op <plyska1993@gmail.com>
2016-10-16 17:14:16 +0300

    test

--- src/test/java/preparation/BeforeClass.java
+++ src/test/java/preparation/BeforeClass.java
@@ -4,5 +4,4 @@ package preparation;
  * Created by Sacred on 16.10.2016.
  */
 public class BeforeClass {
-    ololo
 }
---
+++ src/test/java/tes.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Oleh on 16.10.2016.
+ */
+public class tes {
+}
